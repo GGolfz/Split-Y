59da64a031ea3cc7fd4a064f00e252e50320b6b7
GGolfz <wisarutgolf11@gmail.com>
2024-05-13 21:58:35 +0700

    remove hello

--- server/src/utils/flexMessageHelper.ts
+++ server/src/utils/flexMessageHelper.ts
@@ -18,13 +18,6 @@ const buildGroupBubble = (group: Group) => {
   const LINE_LIFF_URL = process.env.LINE_LIFF_URL;
   return {
     type: "bubble",
-    hero: {
-      type: "image",
-      url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-      size: "full",
-      aspectRatio: "20:13",
-      aspectMode: "cover",
-    },
     body: {
       type: "box",
       layout: "vertical",
